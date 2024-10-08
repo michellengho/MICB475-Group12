19378dcb93b90764fd4f6fa61b4d85486f6660a1
sherinay23 <Sherinaymahsut@gmail.com>
2024-10-07 20:04:05 -0700

    new ryan metadata code

---
+++ project 2/ryan_metadata_code.R
@@ -0,0 +1,12 @@
+library(tidyverse)
+
+# Load metadata
+ryan_meta <- read_delim(file = "project 2/ryan_metadata.tsv", delim = "\t")
+ryan_meta
+
+#unite condition and smoking status column
+ryanmeta_new <- unite(ryan_meta, col="combined condition", `Condition`, `Smoking.status`, sep = "/", remove = FALSE)
+ryanmeta_new
+
+write.table(ryanmeta_new, file = "new_ryan_metadata.tsv", sep = "\t", row.names = FALSE, col.names = TRUE)
+
